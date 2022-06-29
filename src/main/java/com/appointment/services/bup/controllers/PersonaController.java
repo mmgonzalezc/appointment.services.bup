ac8a4e8eb78da78dd670e24d6433c485d4bdfb36
mmgonzalezc <mgonzalezclara@gmail.com>
2022-06-28 20:36:07 -0500

    Persona

--- src/main/java/com/appointment/services/bup/controllers/PersonaController.java
+++ src/main/java/com/appointment/services/bup/controllers/PersonaController.java
@@ -11,10 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -34,8 +31,18 @@ public class PersonaController {
     })
     @PostMapping("${savePersona}")
     public ResponseEntity<ResponseApi> savePersona(@RequestBody Persona persona) {
-        log.info("Save usuario ...");
+        log.info("Guardando registro usuario ...");
         Persona savePersona = personaService.savePersona(persona);
         return new ResponseEntity<>(new ResponseApi(HttpStatus.CREATED.value(), "Operacion exitosa", savePersona), HttpStatus.CREATED);
+    }
+    /**
+     * Valida si es invitado
+     */
+    @GetMapping("${findPersonaByCelular}")
+    public ResponseEntity<ResponseApi> validaInvitado(@PathVariable String celular){
+    log.info("Buscando invitado");
+    log.info("Ir a consultar bd para ir a ver si fue invitado");
+    return new ResponseEntity<>(new ResponseApi(HttpStatus.OK.value(), "Operacion exitosa", savePersona), HttpStatus.CREATED);
+
     }
 }
--- src/main/java/com/appointment/services/bup/entitys/Persona.java
+++ src/main/java/com/appointment/services/bup/entitys/Persona.java
@@ -1,4 +1,17 @@
 package com.appointment.services.bup.entitys;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("Usuario")
 public class Persona {
+
+    private String afiliacion;// 1 = free, 2 = vip
+    private String afilacionInicio;
+    private String afiliacionFin;
+    private String celular;
+    private String nombreUsuario;
+    private String correo;
+    private String clave;
+
+
 }
